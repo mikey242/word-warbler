b894b8ba0e78d6df8359c2ff63a316ff5d6717d6
Michael Iseard <michael@iseard.media>
2022-01-29 13:41:35 +0100

    Tweak the header and add umami

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ import { ReactComponent as Language } from '../images/language.svg';
 import { ReactComponent as Light } from '../images/light.svg';
 import { ReactComponent as Dark } from '../images/dark.svg';
 import { ReactComponent as Help } from '../images/help.svg';
+import { ReactComponent as Logo } from '../images/logo.svg';
 
 function Bar({ setGameState }) {
   const { t } = useTranslation();
@@ -28,17 +29,19 @@ function Bar({ setGameState }) {
   return (
     <nav className="border-gray-300 dark:border-gray-600 border-b text-gray-600 dark:text-gray-300 mb-5 px-4 sm:px-4 py-2.5 w-full">
       <div className="container flex flex-wrap justify-center items-center mx-auto">
-        <div className="mr-auto flex-1">
+        <div className="mr-auto flex items-center">
+          <Logo width="1.5em" />
+          <h1 className="block ml-2 py-2 text-md md:text-xl">Word Warbler</h1>
+        </div>
+        <div className="ml-auto flex justify-end items-center">
           <button
             type="button"
+            className="mr-3"
             aria-label={t('Show help')}
             onClick={() => setGameState((prev) => ({ ...prev, showIntro: true }))}
           >
             <Help width="1.5em" />
           </button>
-        </div>
-        <h1 className="block py-2 px-3 text-xl">Word Warbler</h1>
-        <div className="ml-auto flex-1 flex justify-end items-center text-right">
           <button
             type="button"
             aria-label={t('Toggle dark mode')}
