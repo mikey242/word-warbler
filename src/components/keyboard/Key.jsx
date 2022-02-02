8d607c51a53fc08595656730787bfd4be679cab2
Michael Iseard <michael@iseard.media>
2022-02-02 11:04:34 +0100

    Improve contrast in light mode

--- src/components/keyboard/Key.jsx
+++ src/components/keyboard/Key.jsx
@@ -14,7 +14,7 @@ function Key({
       onClick={() => onClick(character)}
       className={
             classNames(
-              incorrect ? 'bg-gray-600' : 'bg-gray-900',
+              incorrect ? 'dark:bg-gray-600 dark:text-white bg-gray-300 text-gray-600' : 'bg-gray-900',
               'key flex items-center justify-center cursor-pointer text-white mr-1 h-10 min-w-0 p-4 mb-1 rounded-md',
             )
 }
